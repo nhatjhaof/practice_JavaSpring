a1284156d1fd3518ecba02f9d18a5e6987b19602
Your Name <you@example.com>
2024-11-16 18:26:59 +0700

    project basic

---
+++ src/test/java/com/example/practice_Java/PracticeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
